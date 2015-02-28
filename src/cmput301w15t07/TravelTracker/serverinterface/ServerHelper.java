bb4db9caf598c68e6575151f8d2adacda34c6926
ryant26 <ryant26@gmail.com>
2015-02-28 00:20:34 -0700

    Server Interface Skeleton Code #58

--- src/cmput301w15t07/TravelTracker/serverinterface/ServerHelper.java
+++ src/cmput301w15t07/TravelTracker/serverinterface/ServerHelper.java
@@ -21,6 +21,85 @@ package cmput301w15t07.TravelTracker.serverinterface;
  *  limitations under the License.
  */
 
-public class ServerHelper {
+import java.util.Collection;
+import java.util.UUID;
 
+import cmput301w15t07.TravelTracker.model.Claim;
+import cmput301w15t07.TravelTracker.model.Document;
+import cmput301w15t07.TravelTracker.model.Item;
+import cmput301w15t07.TravelTracker.model.Tag;
+import cmput301w15t07.TravelTracker.model.User;
+import io.searchbox.client.*;
+
+public class ServerHelper {
+	private JestClient conn;
+	private Cache cache;
+	
+	/**
+	 * Performs all the connection setup for this class to 
+	 * connect to the ES cluster.
+	 */
+	private void getConnection(){
+		
+	}
+	
+	/**
+	 * This method deletes all passed documents from the ES cluster.
+	 * When finished or on error will use the callback. 
+	 * @param documents
+	 * @param callback
+	 */
+	public void deleteDocuments(Collection<Document> documents, ResultCallback<Collection<Document>> callback){
+		
+	}
+	
+	/**
+	 * Gets all claims for the passed user from the server asynchronously, returns it via 
+	 * ResultCallback
+	 * @param user
+	 * @param callback
+	 */
+	public void getClaims(UUID user, ResultCallback<Collection<Claim>> callback){
+		
+	}
+	
+	/**
+	 * Gets all expense items for the passed claim from the server asynchronously, returns it
+	 * via ResultCallback
+	 * @param claim
+	 * @param callback
+	 */
+	public void getExpenses(UUID claim, ResultCallback<Collection<Item>> callback){
+		
+	}
+	
+	/**
+	 * Gets all tags for the passed user from the server asynchronously, returns it
+	 * via ResultCallback
+	 * @param user
+	 * @param callback
+	 */
+	public void getTags(UUID user, ResultCallback<Collection<Tag>> callback){
+		
+	}
+	
+	/**
+	 * Gets gets user for passed name from the server asynchronously, returns it
+	 * via ResultCallback
+	 * @param name
+	 * @param callback
+	 */
+	public void getUser(String name, ResultCallback<User> callback){
+		
+	}
+	
+	/**
+	 * Saves passed documents to the server asynchronously, returns the documents that
+	 * were successfully saved via callback 
+	 * @param documents
+	 * @param callback
+	 */
+	public void saveDocuments(Collection<Document> documents, ResultCallback<Collection<Document>> callback){
+		
+	}
 }

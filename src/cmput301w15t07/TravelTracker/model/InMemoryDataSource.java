b629fb21cd0e05310ccfb52196ee741079d505b6
kdbanman <kdbanman@ualberta.ca>
2015-03-07 18:02:33 -0700

    document and license InMemoryDataSource.

--- src/cmput301w15t07/TravelTracker/model/InMemoryDataSource.java
+++ src/cmput301w15t07/TravelTracker/model/InMemoryDataSource.java
@@ -1,5 +1,26 @@
 package cmput301w15t07.TravelTracker.model;
 
+/*
+ *   Copyright 2015 Kirby Banman,
+ *                  Stuart Bildfell,
+ *                  Elliot Colp,
+ *                  Christian Ellinger,
+ *                  Braedy Kuzma,
+ *                  Ryan Thornhill
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -10,6 +31,13 @@ import cmput301w15t07.TravelTracker.serverinterface.ResultCallback;
 import cmput301w15t07.TravelTracker.util.Observable;
 import cmput301w15t07.TravelTracker.util.Observer;
 
+/**
+ * Mock data source for early stages of development and unit testing.  Intended for use
+ * by dependency injection.
+ * 
+ * @author colp
+ *
+ */
 public class InMemoryDataSource extends Observable<InMemoryDataSource> implements DataSource, Observer<Document> {
 
 	private HashMap<UUID, Claim> claims;

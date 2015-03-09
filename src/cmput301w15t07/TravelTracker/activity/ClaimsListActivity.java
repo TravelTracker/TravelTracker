5d2b03257da77067dc9313b5403e5143c8c5f30c
ryant26 <ryant26@gmail.com>
2015-03-08 20:50:29 -0600

    Adapter Initial commit

--- src/cmput301w15t07/TravelTracker/activity/ClaimsListActivity.java
+++ src/cmput301w15t07/TravelTracker/activity/ClaimsListActivity.java
@@ -21,11 +21,27 @@ package cmput301w15t07.TravelTracker.activity;
  *  limitations under the License.
  */
 
+import io.searchbox.indices.aliases.AddAliasMapping;
+
+import java.util.Collection;
+import java.util.Currency;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import cmput301w15t07.TravelTracker.R;
+import cmput301w15t07.TravelTracker.model.Claim;
+import cmput301w15t07.TravelTracker.model.Destination;
+import cmput301w15t07.TravelTracker.model.InMemoryDataSource;
 import cmput301w15t07.TravelTracker.model.UserData;
+import cmput301w15t07.TravelTracker.util.Observer;
 
 
 /**
@@ -34,9 +50,10 @@ import cmput301w15t07.TravelTracker.model.UserData;
  * @author kdbanman, colp
  *
  */
-public class ClaimsListActivity extends Activity {
+public class ClaimsListActivity extends Activity implements Observer<InMemoryDataSource> {
 	/** String used to retrieve user data from intent */
 	public static final String USER_DATA = "cmput301w15t07.TravelTracker.userData";
+	
 	
 	/** Data about the logged-in user. */
 	private UserData userData;
@@ -57,4 +74,60 @@ public class ClaimsListActivity extends Activity {
         Bundle bundle = getIntent().getExtras();
         userData = (UserData) bundle.getSerializable(USER_DATA);
 	}
+	
+	@Override
+	public void update(InMemoryDataSource observable) {
+		updateUI();
+	}
+	
+	public void updateUI(){
+		//TODO do all UI tasks here
+	}
+	
+	private class ClaimAdapter extends ArrayAdapter<Claim>{
+
+		public ClaimAdapter(Context context) {
+			super(context, R.layout.claims_list_row_item);
+		}
+		
+		/**
+		 * This function rebuilds the list with the passed claims
+		 * @param claims
+		 */
+		public void rebuildList(Claim [] claims){
+			//possible performance bottleneck
+			clear();
+			addAll(claims);
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View workingView;
+			if (convertView != null){
+				workingView = convertView;
+			} else {
+				LayoutInflater inflator = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				workingView = inflator.inflate(R.layout.claims_list_row_item, parent, false);
+			}
+			
+			// Get all fields 
+			TextView name = (TextView) workingView.findViewById(R.id.claimsListRowItemName);
+			TextView date = (TextView) workingView.findViewById(R.id.claimsListRowItemDate);
+			LinearLayout destinationContainer = (LinearLayout) workingView.findViewById(R.id.claimsListDestinationContainer);
+			LinearLayout totalsContainer = (LinearLayout) workingView.findViewById(R.id.claimsListTotalContainer);
+			
+			
+			
+			return workingView;
+		}
+		
+		public void addTotal(Float amt,Currency currency){
+			//TODO implement the dynamic addition of total textviews
+		}
+		
+		public void addDestination(Destination dest){
+			//TODO implement the dynamic addition of destination textviews
+		}
+	}
+
 }

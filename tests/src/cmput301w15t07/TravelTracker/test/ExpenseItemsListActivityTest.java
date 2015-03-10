e9d82333b150d2c9855cd8b4a01b44f7403d61cd
kuzi117 <braedy@ualberta.ca>
2015-03-09 18:43:35 -0600

    Did tests setup and test preconditions.

--- tests/src/cmput301w15t07/TravelTracker/test/ExpenseItemsListActivityTest.java
+++ tests/src/cmput301w15t07/TravelTracker/test/ExpenseItemsListActivityTest.java
@@ -21,21 +21,116 @@ package cmput301w15t07.TravelTracker.test;
  *  limitations under the License.
  */
 
+import java.util.Collection;
+
 import cmput301w15t07.TravelTracker.activity.ExpenseItemsListActivity;
-import android.test.ActivityInstrumentationTestCase2;
+import cmput301w15t07.TravelTracker.model.Claim;
+import cmput301w15t07.TravelTracker.model.DataSource;
+import cmput301w15t07.TravelTracker.model.GeneratedDataSource;
+import cmput301w15t07.TravelTracker.model.User;
+import cmput301w15t07.TravelTracker.model.UserData;
+import cmput301w15t07.TravelTracker.model.UserRole;
+import cmput301w15t07.TravelTracker.testutils.MockTravelTrackerApp;
+import cmput301w15t07.TravelTracker.testutils.SynchronizedResultCallback;
+import cmput301w15t07.TravelTracker.util.ItemsListAdapter;
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+import android.util.Log;
+import android.widget.ListView;
 
 /**
  * Test for expense item list activities.
  * 
  * Each relevant Use Case UC.XxxYyy is tested with method testXxxYyy()
  * 
- * @author kdbanman
+ * @author kdbanman,
+ *         braedy
  *
  */
-public class ExpenseItemsListActivityTest extends ActivityInstrumentationTestCase2<ExpenseItemsListActivity> {
+public class ExpenseItemsListActivityTest extends ActivityUnitTestCase<ExpenseItemsListActivity> {
+    DataSource dataSource;
+    Instrumentation instrumentation;
+    Activity activity;
+    
+    User user;
+    UserData userData;
+    Collection<Claim> allClaims;
+    Claim claim = null; // The specific claim we'll use items from
+    
+    ListView itemsList;
+    ItemsListAdapter adapter;
+    
+    private static final String LOG_TAG = "ExpenseItemsListTest";
+    
 
 	public ExpenseItemsListActivityTest() {
 		super(ExpenseItemsListActivity.class);
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+	    super.setUp();
+	    // Use generated data.
+        dataSource = new GeneratedDataSource();
+        
+        MockTravelTrackerApp app = new MockTravelTrackerApp();
+        setApplication(app);
+        app.onCreate(dataSource);
+        
+        
+        instrumentation = getInstrumentation();
+
+        // Get User
+        SynchronizedResultCallback<User> userCallback = new SynchronizedResultCallback<User>();
+        dataSource.addUser(userCallback);
+        boolean success = userCallback.waitForResult();
+        if (!success) {
+            throw new RuntimeException("Couldn't add User, how can I test without a User?");
+        }
+        user = userCallback.getResult();
+        userData = new UserData(user.getUUID(), user.getUserName(), UserRole.CLAIMANT);
+        
+        // Get all Claims
+        SynchronizedResultCallback<Collection<Claim>> claimCallback = new SynchronizedResultCallback<Collection<Claim>>();
+        dataSource.getAllClaims(claimCallback);
+        success = claimCallback.waitForResult();
+        if (!success) {
+            throw new RuntimeException("Couldn't get Claims, why no Claims?");
+        }
+        allClaims = claimCallback.getResult();
+        Log.d(LOG_TAG, "Length: " + allClaims.size());
+        
+        if (allClaims.isEmpty()) {
+            throw new RuntimeException("Got no claims back.");
+        }
+        
+        for (Claim c : allClaims) {
+            if (c.getUser() == user.getUUID()) {
+                claim = c;
+                Log.d("ExpenseItemsListTest", c.toString());
+                break;
+            }
+        }
+        if (claim == null) {
+            throw new RuntimeException("No claim for added user...");
+        }
+        
+        Intent intent = new Intent(instrumentation.getTargetContext(), ExpenseItemsListActivity.class);
+        intent.putExtra(ExpenseItemsListActivity.USER_DATA, userData);
+        intent.putExtra(ExpenseItemsListActivity.CLAIM_UUID, claim.getUUID());
+        startActivity(intent, null, null);
+
+        activity = getActivity();
+        itemsList = (ListView) activity.findViewById(cmput301w15t07.TravelTracker.R.id.itemsListListView);
+        adapter = (ItemsListAdapter) itemsList.getAdapter();
+	}
+	
+	public void testPreconditions() {
+	    assertNotNull("Activity was null.", activity);
+	    assertNotNull("ListView was null.", itemsList);
+	    assertNotNull("Adapter was null.", adapter);
 	}
 	
 	public void testListExpenseItems() {

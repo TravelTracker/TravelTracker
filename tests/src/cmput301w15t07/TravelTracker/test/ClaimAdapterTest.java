34185fa2254f2acbddd5247a237e754225bda51d
ryant26 <ryant26@gmail.com>
2015-03-12 22:48:17 -0600

    added some claim adapter tests added a testing utility to minimize use of synchronize callbacks

---
+++ tests/src/cmput301w15t07/TravelTracker/test/ClaimAdapterTest.java
@@ -0,0 +1,42 @@
+package cmput301w15t07.TravelTracker.test;
+
+import cmput301w15t07.TravelTracker.model.GeneratedDataSource;
+import cmput301w15t07.TravelTracker.model.UserRole;
+import cmput301w15t07.TravelTracker.testutils.AllCallbacks;
+import cmput301w15t07.TravelTracker.util.ClaimAdapter;
+import android.test.AndroidTestCase;
+
+public class ClaimAdapterTest extends AndroidTestCase{
+	
+	GeneratedDataSource ds;
+	ClaimAdapter adapter;
+	AllCallbacks acb;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		ds = new GeneratedDataSource();
+		adapter = new ClaimAdapter(getContext());
+		acb = new AllCallbacks(ds);
+	}
+	
+	public void testCLaimsSortedClaimant(){
+		adapter.rebuildList(acb.getClaims(), acb.getItems(), UserRole.CLAIMANT);
+		 for (int i = 1; i < adapter.getCount(); i++){
+			 assertTrue(adapter.getItem(i).getStartDate().compareTo(adapter.getItem(i-1).getStartDate()) < 1);
+		 }
+	}
+	
+	public void testCLaimsSortedApprover(){
+		adapter.rebuildList(acb.getClaims(), acb.getItems(), UserRole.APPROVER);
+		 for (int i = 1; i < adapter.getCount(); i++){
+			 assertTrue(adapter.getItem(i).getStartDate().compareTo(adapter.getItem(i-1).getStartDate()) > -1);
+		 }
+	}
+	
+	public void testAdapterMappingDS(){
+		adapter.rebuildList(acb.getClaims(), acb.getItems(), UserRole.APPROVER);
+		assertEquals(acb.getClaims().size(), adapter.getCount());
+	}
+	
+}
---
+++ tests/src/cmput301w15t07/TravelTracker/testutils/AllCallbacks.java
@@ -0,0 +1,94 @@
+package cmput301w15t07.TravelTracker.testutils;
+
+/*
+ *   Copyright 2015 Kirby Banman,
+ *                  Stuart Bildfell,
+ *                  Elliot Colp,
+ *                  Christian Ellinger,
+ *                  Braedy Kuzma,
+ *                  Ryan Thornhill
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import java.util.Collection;
+
+import android.test.AndroidTestCase;
+import cmput301w15t07.TravelTracker.model.Claim;
+import cmput301w15t07.TravelTracker.model.DataSource;
+import cmput301w15t07.TravelTracker.model.Item;
+import cmput301w15t07.TravelTracker.model.Tag;
+import cmput301w15t07.TravelTracker.model.User;
+
+
+/**
+ * This is a convenience class for testing to get all of the data you might need from a datasource
+ *  for a test without messing around with synchronized call backs. 
+ * 
+ * Please add to it as you see fit!
+ * @author ryan
+ *
+ */
+public class AllCallbacks extends AndroidTestCase{
+	public SynchronizedResultCallback<User> userCallback = new SynchronizedResultCallback<User>();
+	public SynchronizedResultCallback<Collection<Claim>> claimCallback = new SynchronizedResultCallback<Collection<Claim>>();
+	public SynchronizedResultCallback<Collection<Item>> itemCallback = new SynchronizedResultCallback<Collection<Item>>();
+	public SynchronizedResultCallback<Collection<User>> usersCallback = new SynchronizedResultCallback<Collection<User>>();
+	public SynchronizedResultCallback<Collection<Tag>> tagCallback = new SynchronizedResultCallback<Collection<Tag>>();
+	
+	/**
+	 *  Constructor for this class 
+	 * @param ds - The datasource you want to use
+	 */
+	public AllCallbacks(DataSource ds){
+		ds.getAllClaims(claimCallback);
+		ds.getAllItems(itemCallback);
+		ds.addUser(userCallback);
+		ds.getAllTags(tagCallback);
+		ds.getAllUsers(usersCallback);
+	}
+	
+	
+	public User getUser(){
+		return getData(userCallback);
+	}
+	
+	public Collection<User> getUsers(){
+		return getData(usersCallback);
+	}
+	
+	public Collection<Claim> getClaims(){
+		return getData(claimCallback);
+	}
+	
+	public Collection<Item> getItems(){
+		return getData(itemCallback);
+	}
+	
+	public Collection<Tag> getTags(){
+		return getData(tagCallback);
+	}
+	
+	
+	private <T> T getData(SynchronizedResultCallback<T> callback) {
+		try{
+			boolean success = callback.waitForResult();
+			assertTrue("Failure adding new user to gen ds", success);
+			return callback.getResult();
+		} catch (InterruptedException e){
+			fail("failed to get data from data source for class: " + callback.toString());
+		}
+		return null;
+	}
+}
+

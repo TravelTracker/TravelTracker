fd83cf05d760a0f6dccd6fdba431c27dfb860af4
Elliot Colp <thelastbanana@gmail.com>
2015-03-15 16:10:11 -0600

    Added missing documentation

--- src/cmput301w15t07/TravelTracker/model/Item.java
+++ src/cmput301w15t07/TravelTracker/model/Item.java
@@ -69,65 +69,137 @@ public class Item extends Document {
 		
 	}
 	
+	/**
+	 * Get the claim to which this belongs.
+	 * @return The claim's UUID.
+	 */
 	public UUID getClaim() {
 		return this.claim;
 	}
+	
+	/**
+	 * Set the claim to which this belongs.
+	 * @param claim The claim's UUID.
+	 */
 	public void setClaim(UUID claim) {
 		this.claim = claim;
 		this.updateObservers(this);
 	}
 	
+	/**
+	 * Get the item description.
+	 * @return The description.
+	 */
 	public String getDescription() {
 		return description;
 	}
+	
+	/**
+	 * Set the item description.
+	 * @param description The description.
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 		this.updateObservers(this);
 	}
 	
+	/**
+	 * Get the item's category.
+	 * @return The category.
+	 */
 	public ItemCategory getCategory() {
 		return category;
 	}
+	
+	/**
+	 * Set the item's category.
+	 * @param category The category.
+	 */
 	public void setCategory(ItemCategory category) {
 		this.category = category;
 		this.updateObservers(this);
 	}
 	
+	/**
+	 * Get the date on which the expense item occurred.
+	 * @return The date.
+	 */
 	public Date getDate() {
 		return date;
 	}
+	
+	/**
+	 * Set the date on which the expense item occurred.
+	 * @param date The date.
+	 */
 	public void setDate(Date date) {
 		this.date = date;
 		this.updateObservers(this);
 	}
 	
+	/**
+	 * Get the expense amount.
+	 * @return The amount.
+	 */
 	public Float getAmount() {
 		return amount;
 	}
+	
+	/**
+	 * Set the expense amount.
+	 * @param amount The amount.
+	 */
 	public void setAmount(Float amount) {
 		this.amount = amount;
 		this.updateObservers(this);
 	}
 	
+	/**
+	 * Get the currency type of the expense.
+	 * @return The currency.
+	 */
 	public Currency getCurrency() {
 		return currency;
 	}
+	
+	/**
+	 * Set the currency type of the expense.
+	 * @param currency The currency.
+	 */
 	public void setCurrency(Currency currency) {
 		this.currency = currency;
 		this.updateObservers(this);
 	}
 	
+	/**
+	 * Get the attached Receipt.
+	 * @return The receipt.
+	 */
 	public Receipt getReceipt() {
 		return receipt;
 	}
+	
+	/**
+	 * Set the attached Receipt.
+	 * @param receipt The receipt.
+	 */
 	public void setReceipt(Receipt receipt) {
 		this.receipt = receipt;
 		this.updateObservers(this);
 	}
+	
+	/**
+	 * Get the status of the expense item.
+	 * @return The status.
+	 */
 	public boolean getStatus() {
 		return status;
 	}
-
+	
+	/**
+	 * Set the status of the expense item.
+	 * @param status The status.
+	 */
 	public void setStatus(boolean status) {
 		this.status = status;
 		this.updateObservers(this);

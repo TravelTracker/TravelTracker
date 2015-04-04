2aaced81b4f7645071901ae1e846581ed038b26c
kdbanman <kdbanman@ualberta.ca>
2015-04-04 16:46:05 -0600

    heavily test file system cache

--- tests/src/cmput301w15t07/TravelTracker/test/util/FileSystemHelperTest.java
+++ tests/src/cmput301w15t07/TravelTracker/test/util/FileSystemHelperTest.java
@@ -6,8 +6,13 @@ import java.util.UUID;
 
 import cmput301w15t07.TravelTracker.model.Claim;
 import cmput301w15t07.TravelTracker.model.DataSource;
+import cmput301w15t07.TravelTracker.model.Document;
+import cmput301w15t07.TravelTracker.model.Geolocation;
 import cmput301w15t07.TravelTracker.model.InMemoryDataSource;
 import cmput301w15t07.TravelTracker.model.Item;
+import cmput301w15t07.TravelTracker.model.ItemCategory;
+import cmput301w15t07.TravelTracker.model.ItemCurrency;
+import cmput301w15t07.TravelTracker.model.Receipt;
 import cmput301w15t07.TravelTracker.model.Tag;
 import cmput301w15t07.TravelTracker.model.User;
 import cmput301w15t07.TravelTracker.serverinterface.FileSystemHelper;
@@ -32,7 +37,13 @@ public class FileSystemHelperTest extends InstrumentationTestCase {
 	ArrayList<User> justSteve;
 
 	ArrayList<Claim> claims;
-	ArrayList<Claim> justClaim2;
+	ArrayList<Claim> claim2;
+
+	ArrayList<Item> items;
+	ArrayList<Item> item2;
+
+	ArrayList<Tag> tags;
+	ArrayList<Tag> tag2;
 	
 	@Override
 	protected void setUp() throws Exception {
@@ -44,18 +55,38 @@ public class FileSystemHelperTest extends InstrumentationTestCase {
 		
 		u1 = DataSourceUtils.addUser("Billington", ds);
 		u2 = DataSourceUtils.addUser("Stevula", ds);
+		
 		c1 = DataSourceUtils.addEmptyClaim(u1, ds);
+		c1.addComment("test comment");
+		c1.setApprover(u2.getUUID());
 		c2 = DataSourceUtils.addEmptyClaim(u1, ds);
+		
 		i1 = DataSourceUtils.addEmptyItem(c1, ds);
+		i1.setComplete(true);
+		i1.setCurrency(ItemCurrency.CNY);
+		i1.setDescription("no words");
+		i1.setGeolocation(new Geolocation(100.3, 97.3));
+		i1.setCategory(ItemCategory.AIR_FARE);
+		i1.setReceipt(new Receipt());
 		i2 = DataSourceUtils.addEmptyItem(c1, ds);
+		
 		t1 = DataSourceUtils.addEmptyTag(u1, ds);
+		t1.setTitle("Noodles.");
 		t2 = DataSourceUtils.addEmptyTag(u1, ds);
 
 		users = new ArrayList<User>(Arrays.asList(u1, u2));
 		justSteve = new ArrayList<User>(Arrays.asList(u2));
 
 		claims = new ArrayList<Claim>(Arrays.asList(c1, c2));
-		justClaim2 = new ArrayList<Claim>(Arrays.asList(c2));
+		claim2 = new ArrayList<Claim>(Arrays.asList(c2));
+
+		items = new ArrayList<Item>(Arrays.asList(i1, i2));
+		item2 = new ArrayList<Item>(Arrays.asList(i2));
+
+		tags = new ArrayList<Tag>(Arrays.asList(t1, t2));
+		tag2 = new ArrayList<Tag>(Arrays.asList(t2));
+
+		c1.setTags(new ArrayList<UUID>(Arrays.asList(t1.getUUID(), t2.getUUID())));
 	}
 	
 	@Override
@@ -103,7 +134,7 @@ public class FileSystemHelperTest extends InstrumentationTestCase {
 		assertEquals(0, fs.getClaims(u2.getUUID()).size());
 		assertEquals(0, fs.getClaims(UUID.randomUUID()).size());
 		
-		fs.deleteDocuments(justClaim2);
+		fs.deleteDocuments(claim2);
 		assertTrue(fs.getClaims(u1.getUUID()).contains(c1));
 		assertTrue(!fs.getClaims(u1.getUUID()).contains(c2));
 		assertEquals(0, fs.getClaims(u2.getUUID()).size());
@@ -116,15 +147,107 @@ public class FileSystemHelperTest extends InstrumentationTestCase {
 		assertEquals(0, fs.getClaims(UUID.randomUUID()).size());
 	}
 	
-	public void testAddItem() {
+	public void testItems() throws Exception {
+
+		fs.<User>saveDocuments(users);
+		fs.<Claim>saveDocuments(claims);
 		
+		fs.deleteDocuments(items);
+		assertEquals(0, fs.getExpenses(c1.getUUID()).size());
+		assertEquals(0, fs.getExpenses(c2.getUUID()).size());
+		assertEquals(0, fs.getExpenses(u2.getUUID()).size());
+		assertEquals(0, fs.getExpenses(UUID.randomUUID()).size());
+
+		fs.<Item>saveDocuments(items);
+		assertTrue(fs.getExpenses(c1.getUUID()).contains(i1));
+		assertTrue(fs.getExpenses(c1.getUUID()).contains(i2));
+		assertEquals(2, fs.getExpenses(c1.getUUID()).size());
+		assertEquals(0, fs.getExpenses(c2.getUUID()).size());
+		assertEquals(0, fs.getExpenses(UUID.randomUUID()).size());
+		
+		fs.deleteDocuments(item2);
+		assertTrue(fs.getExpenses(c1.getUUID()).contains(i1));
+		assertTrue(!fs.getExpenses(c1.getUUID()).contains(i2));
+		assertEquals(0, fs.getExpenses(c2.getUUID()).size());
+		assertEquals(0, fs.getExpenses(UUID.randomUUID()).size());
+		
+		fs.deleteDocuments(items);
+		assertTrue(!fs.getExpenses(c1.getUUID()).contains(i1));
+		assertTrue(!fs.getExpenses(c1.getUUID()).contains(i2));
+		assertEquals(0, fs.getExpenses(c2.getUUID()).size());
+		assertEquals(0, fs.getExpenses(UUID.randomUUID()).size());
 	}
 	
-	public void testAddTag() {
+	public void testTags() throws Exception {
+
+		fs.<User>saveDocuments(users);
+		fs.<Claim>saveDocuments(claims);
+		fs.<Item>saveDocuments(items);
 		
+		fs.deleteDocuments(tags);
+		assertEquals(0, fs.getTags(u1.getUUID()).size());
+		assertEquals(0, fs.getTags(u2.getUUID()).size());
+		assertEquals(0, fs.getTags(u2.getUUID()).size());
+		assertEquals(0, fs.getTags(UUID.randomUUID()).size());
+
+		fs.<Tag>saveDocuments(tags);
+		assertTrue(fs.getTags(u1.getUUID()).contains(t1));
+		assertTrue(fs.getTags(u1.getUUID()).contains(t2));
+		assertEquals(2, fs.getTags(u1.getUUID()).size());
+		assertEquals(0, fs.getTags(u2.getUUID()).size());
+		assertEquals(0, fs.getTags(UUID.randomUUID()).size());
+		
+		fs.deleteDocuments(tag2);
+		assertTrue(fs.getTags(u1.getUUID()).contains(t1));
+		assertTrue(!fs.getTags(u1.getUUID()).contains(t2));
+		assertEquals(0, fs.getTags(u2.getUUID()).size());
+		assertEquals(0, fs.getTags(UUID.randomUUID()).size());
+		
+		fs.deleteDocuments(tags);
+		assertTrue(!fs.getTags(u1.getUUID()).contains(t1));
+		assertTrue(!fs.getTags(u1.getUUID()).contains(t2));
+		assertEquals(0, fs.getTags(u2.getUUID()).size());
+		assertEquals(0, fs.getTags(UUID.randomUUID()).size());
 	}
 	
-	public void testAddMixed() {
+	public void testDeleteMixed() throws Exception {
+		fs.<User>saveDocuments(users);
+		fs.<Claim>saveDocuments(claims);
+		fs.<Item>saveDocuments(items);
+		fs.<Tag>saveDocuments(tags);
 		
+		assertEquals(2, fs.getClaims(u1.getUUID()).size());
+		assertEquals(2, fs.getExpenses(c1.getUUID()).size());
+		assertEquals(2, fs.getTags(u1.getUUID()).size());
+		assertEquals(u1, fs.getUser("Billington"));
+		assertEquals(u2, fs.getUser("Stevula"));
+		
+		fs.deleteDocuments(new ArrayList<Document>(Arrays.asList(u1, c1, t1, i1)));
+		assertEquals(1, fs.getClaims(u1.getUUID()).size());
+		assertEquals(1, fs.getExpenses(c1.getUUID()).size());
+		assertEquals(1, fs.getTags(u1.getUUID()).size());
+		assertNull(fs.getUser("Billington"));
+		assertEquals(u2, fs.getUser("Stevula"));
+		
+		fs.deleteDocuments(new ArrayList<Document>(Arrays.asList(u1, c1, t1, i1)));
+		assertEquals(1, fs.getClaims(u1.getUUID()).size());
+		assertEquals(1, fs.getExpenses(c1.getUUID()).size());
+		assertEquals(1, fs.getTags(u1.getUUID()).size());
+		assertNull(fs.getUser("Billington"));
+		assertEquals(u2, fs.getUser("Stevula"));
+		
+		fs.deleteDocuments(new ArrayList<Document>(Arrays.asList(u2, c2, t1, i1))); // mixed already deleted, not deleted
+		assertEquals(0, fs.getClaims(u1.getUUID()).size());
+		assertEquals(1, fs.getExpenses(c1.getUUID()).size());
+		assertEquals(1, fs.getTags(u1.getUUID()).size());
+		assertNull(fs.getUser("Billington"));
+		assertNull(fs.getUser("Stevula"));
+		
+		fs.deleteDocuments(new ArrayList<Document>(Arrays.asList(u1, c1, t2, i2))); // all remaining
+		assertEquals(0, fs.getClaims(u1.getUUID()).size());
+		assertEquals(0, fs.getExpenses(c1.getUUID()).size());
+		assertEquals(0, fs.getTags(u1.getUUID()).size());
+		assertNull(fs.getUser("Billington"));
+		assertNull(fs.getUser("Stevula"));
 	}
 }

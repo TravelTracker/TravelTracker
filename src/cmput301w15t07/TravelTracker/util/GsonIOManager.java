ae5c20e9b2b3e4e634d9416c07a171689e3d6780
kdbanman <kdbanman@ualberta.ca>
2015-04-04 16:05:07 -0600

    fix accuracy issue with custom date adapter for gson.

--- src/cmput301w15t07/TravelTracker/util/GsonIOManager.java
+++ src/cmput301w15t07/TravelTracker/util/GsonIOManager.java
@@ -28,12 +28,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
+import java.util.Date;
 
 import android.content.Context;
 import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 /**
  * Facade to wrap streams and GSON ugliness.
@@ -48,7 +56,7 @@ public class GsonIOManager {
 	
 	public GsonIOManager(Context ctx) {
 		this.ctx = ctx;
-		gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();;
+		gson = new GsonBuilder().registerTypeHierarchyAdapter(Date.class, new DateAdapter()).serializeNulls().create();;
 	}
 	
 	public <T> T load(String filename, Type type) throws FileNotFoundException {
@@ -82,4 +90,24 @@ public class GsonIOManager {
 			Log.e("GSONIOManager", e.getMessage());
 		}
 	}
+	
+	/*
+	 * Custom date gson adapter mix and matched from
+	 * 	http://stackoverflow.com/questions/6873020/gson-date-format
+	 * and from 
+	 *  http://stackoverflow.com/questions/5671373/unparseable-date-1302828677828-trying-to-deserialize-with-gson-a-millisecond
+	 * on 4 April 2015
+	 */
+	private class DateAdapter implements JsonDeserializer<Date>, JsonSerializer<Date> {
+		@Override
+		public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+		      return json == null ? null : new Date(json.getAsJsonPrimitive().getAsLong()); 
+		}
+
+		@Override
+		public JsonElement serialize(Date date, Type typeOfT, JsonSerializationContext context) {
+			return date == null ? null : new JsonPrimitive(date.getTime());
+		}
+	}
+			
 }
